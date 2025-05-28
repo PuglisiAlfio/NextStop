5b54504c432188f9e66eba94e40e402dc2df3ef9
Alfio Puglisi <puglisi_algio@ciao.it>
2025-05-28 09:41:44 +0200

    Fix: gestito correttamente il timer con useEffect per annullare il timeout alla chiusura

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  // Imposta un timer per confermare automaticamente l'eliminazione dopo 3 secondi
-  setTimeout(() => {
-    onConfirm();
-  }, 3000);
+  useEffect(() => {
+    // Imposta un timer per confermare automaticamente l'eliminazione dopo 3 secondi
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+    // Stoppa il timer per annullare l'eliminazione dopo 3 secondi
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Sei sicuro?</h2>
